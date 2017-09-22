187c3643cfc782d3ffc9ddc1928e0fd1bea42c2d
Saoud Rizwan <saoudmriz@gmail.com>
2017-09-22 06:59:56 -0700

    Updated documentation

--- Sources/Disk+VolumeInformation.swift
+++ Sources/Disk+VolumeInformation.swift
@@ -53,7 +53,7 @@ public extension Disk {
     
     /// Volume’s available capacity in bytes for storing important resources.
     ///
-    /// Indicates amount of space that can be made available  for things the user has explicitly requested in app's UI (i.e. downloading a video or new level for a game.)
+    /// Indicates the amount of space that can be made available  for things the user has explicitly requested in the app's UI (i.e. downloading a video or new level for a game.)
     /// If you need more space than what's available - let user know the request cannot be fulfilled.
     public static var availableCapacityForImportantUsage: Int? {
         get {
