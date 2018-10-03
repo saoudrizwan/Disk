bce47d1aff8f0d44b7d9a7573860d705f339e59f
Maksym Savisko <stowyn@gmail.com>
2018-10-03 17:49:35 +0300

    feat: add example multiple support (swift 4 and swift 4.2)

--- DiskExample/DiskExample/AppDelegate.swift
+++ DiskExample/DiskExample/AppDelegate.swift
@@ -12,12 +12,22 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    #if swift(>=4.2)
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil)
+        -> Bool {
+            // Override point for customization after application launch.
+            return true
+    }
+    #else
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+    -> Bool {
         // Override point for customization after application launch.
         return true
     }
+    #endif
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
