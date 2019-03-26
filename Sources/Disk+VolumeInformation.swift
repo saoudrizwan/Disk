2a9047204cd1c8534f13afd0735af61f0b43c450
Daniel Saidi <daniel.saidi@gmail.com>
2019-03-26 15:19:52 +0100

    Upgrade to Swift 5

--- Sources/Disk+VolumeInformation.swift
+++ Sources/Disk+VolumeInformation.swift
@@ -36,7 +36,7 @@ public extension Disk {
     }
     
     /// Volume’s total capacity in bytes.
-    public static var totalCapacity: Int? {
+    static var totalCapacity: Int? {
         get {
             let resourceValues = getVolumeResourceValues(for: .volumeTotalCapacityKey)
             return resourceValues?.volumeTotalCapacity
@@ -44,7 +44,7 @@ public extension Disk {
     }
     
     /// Volume’s available capacity in bytes.
-    public static var availableCapacity: Int? {
+    static var availableCapacity: Int? {
         get {
             let resourceValues = getVolumeResourceValues(for: .volumeAvailableCapacityKey)
             return resourceValues?.volumeAvailableCapacity
@@ -55,7 +55,7 @@ public extension Disk {
     ///
     /// Indicates the amount of space that can be made available  for things the user has explicitly requested in the app's UI (i.e. downloading a video or new level for a game.)
     /// If you need more space than what's available - let user know the request cannot be fulfilled.
-    public static var availableCapacityForImportantUsage: Int? {
+    static var availableCapacityForImportantUsage: Int? {
         get {
             let resourceValues = getVolumeResourceValues(for: .volumeAvailableCapacityForImportantUsageKey)
             if let result = resourceValues?.volumeAvailableCapacityForImportantUsage {
@@ -70,7 +70,7 @@ public extension Disk {
     ///
     /// Indicates the amount of space available for things that the user is likely to want but hasn't explicitly requested (i.e. next episode in video series they're watching, or recently updated documents in a server that they might be likely to open.)
     /// For these types of files you might store them initially in the caches directory until they are actually used, at which point you can move them in app support or documents directory.
-    public static var availableCapacityForOpportunisticUsage: Int? {
+    static var availableCapacityForOpportunisticUsage: Int? {
         get {
             let resourceValues = getVolumeResourceValues(for: .volumeAvailableCapacityForOpportunisticUsageKey)
             if let result = resourceValues?.volumeAvailableCapacityForOpportunisticUsage {
