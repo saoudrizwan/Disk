f5254d8d889448bf13b97527b21674bcf92dfb85
Saoud Rizwan <saoudrizwan@gmail.com>
2022-04-01 19:27:00 -0400

    Revert "Move paid files to .gitroyalty"
    This reverts commit 354be687261ae78fbea0ec830d7f1a0a52c0cdc9.

--- Package.swift
+++ Package.swift
@@ -1,1 +1,23 @@
-.gitroyalty/Package.swift
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "Disk",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(name: "Disk", targets: ["Disk"])
+    ],
+    targets: [
+        .target(
+            name: "Disk",
+            path: "Sources",
+            exclude: ["DiskExample"]
+        ),
+        .testTarget(
+            name: "DiskTests",
+            dependencies: ["Disk"],
+            path: "Tests",
+            exclude:  ["DiskExample"]
+        )
+    ]
+)
